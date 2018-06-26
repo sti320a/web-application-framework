d5295270bd75b470cc4f41328cbc55e294800fba
sti320a <sti320a@gmail.com>
2018-06-26 21:33:36 +0900

    06/26/21:33

---
+++ controller.py
@@ -0,0 +1,20 @@
+#! python3
+# controller.py
+
+from flask import Flask, session, redirect, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def showIndexView():
+    return render_template("index.html")
+
+@app.route('/login')
+def showLoginView():
+    return render_template('login.html')
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='127.0.0.1', port=5000)
