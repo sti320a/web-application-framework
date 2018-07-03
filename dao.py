7af7f6bcf52df3a62a0333e57b5598f76b15d7e5
sti320a <sti320a@gmail.com>
2018-07-03 21:49:32 +0900

    edit dao and add testDao

--- dao.py
+++ dao.py
@@ -1,8 +1,32 @@
 import sqlite3
-conn = sqlite3.connect('./db/user.db')
+import sys, os
 
-c = conn.cursor()
-create_table="CREATE TABLE user (id int, name varchar(64))"
-c.execute(create_table)
-conn.commit()
-conn.close()
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def create():
+    conn = sqlite3.connect(ROOT_DIR+'/db/user.db')
+    c = conn.cursor()
+    create_table="CREATE TABLE IF NOT EXISTS user (id int, name varchar(64))"
+    c.execute(create_table)
+    conn.commit()
+    conn.close()
+
+def insert():
+    conn = sqlite3.connect(ROOT_DIR+'/db/user.db')
+    c = conn.cursor()
+    insert = "INSERT INTO user(id, name) VALUES(?, ?)"
+    place_holder = [2, "Hello world!"]
+    c.execute(insert, place_holder)
+    conn.commit()
+    conn.close()
+
+def selectAll():
+    conn = sqlite3.connect(ROOT_DIR+'/db/user.db')
+    c = conn.cursor()
+    select = "SELECT * FROM user"
+    result = c.execute(select)
+    conn.commit()
+    for row in result:
+        print(row)
+    conn.close()
+    
