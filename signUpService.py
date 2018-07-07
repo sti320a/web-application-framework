8bfc6e1c1e73ac1957b0466e669feb96d42f4a3c
sti320a <sti320a@gmail.com>
2018-07-08 00:37:49 +0900

    add convert2AuthKeyFromPasswordFunction

--- signUpService.py
+++ signUpService.py
@@ -1,16 +1,21 @@
 import dao
+import hashlib
 
 def signUpUserProvisonally(username, email, password):
-    # TODO1 validation check
+    
+    # validation check
     if validationCheck4InsertProvisionalUser(username, email, password) == False:
         print("validation error")
         return False
-
-    # TODO2 password to hash
+    
+    # password to hash
+    auth_key = covert2AuthKeyFromPassword(password)
+    if auth_key == None:
+        return False
 
     dao.create("CREATE TABLE IF NOT EXISTS provisionalUser (id int, name varchar(64), email varchar(64), auth_key varchar(64))", "user.db")
     id = getNewUserId4InsertProvisionalUser()
-    dao.insert("INSERT INTO provisionalUser (id, name, email, auth_key) VALUES (?,?,?,?)", [id, username, email, password], "user.db")
+    dao.insert("INSERT INTO provisionalUser (id, name, email, auth_key) VALUES (?,?,?,?)", [id, username, email, auth_key], "user.db")
     dao.select("SELECT * FROM provisionalUser", "user.db", True)
 
 def getNewUserId4InsertProvisionalUser():
@@ -25,3 +30,9 @@ def validationCheck4InsertProvisionalUser(username, email, password):
     if len(password) <= 8:
         print("Password is too short")
         return False
+    return True
+
+def covert2AuthKeyFromPassword(password):
+    if password == None:
+        return Flase
+    return hashlib.sha256(password.encode('utf-8')).hexdigest()
